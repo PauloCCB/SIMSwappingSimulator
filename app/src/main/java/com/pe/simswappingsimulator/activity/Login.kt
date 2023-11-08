155fd65b8b8713cff890e3c50611c5332017836e
Tomas Campana <tcampana87@gmail.com>
2023-11-08 01:40:23 -0500

    Avances

--- app/src/main/java/com/pe/simswappingsimulator/activity/Login.kt
+++ app/src/main/java/com/pe/simswappingsimulator/activity/Login.kt
@@ -2,11 +2,31 @@ package com.pe.simswappingsimulator.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.pe.simswappingsimulator.Manifest
 import com.pe.simswappingsimulator.R
+import com.pe.simswappingsimulator.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+        }
+
+        val view = binding.root
+        setContentView(view)
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.btnLogin.setOnClickListener {
+
+        }
     }
 }
