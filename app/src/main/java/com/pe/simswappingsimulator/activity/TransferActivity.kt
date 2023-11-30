5a7243fc9cd1159f7a64d2fcdb858adea969ccbe
Tomas <tcampana87@gmail.com>
2023-11-30 11:24:07 -0500

    Avance

---
+++ app/src/main/java/com/pe/simswappingsimulator/activity/TransferActivity.kt
@@ -0,0 +1,17 @@
+package com.pe.simswappingsimulator.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pe.simswappingsimulator.R
+import com.pe.simswappingsimulator.databinding.ActivityTransferBinding
+
+class TransferActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTransferBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTransferBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_transfer)
+    }
+}
